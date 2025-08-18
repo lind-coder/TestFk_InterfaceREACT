6906804624f6108f96ae57fce437a1bf8c660b35
lind-coder <semplicmeentelinda@live.it>
2025-08-18 09:38:15 +0200

    modifyem

--- marketmanager/src/Fetch/FetchShiftsByRange.ts
+++ marketmanager/src/Fetch/FetchShiftsByRange.ts
@@ -6,7 +6,7 @@ export const fetchShiftsByRange = async (
   endDate: string
 ): Promise<Shift[]> => {
   try {
-    console.log("🔍 Invio richiesta con:", { startDate, endDate });
+    console.log(" Invio richiesta con:", { startDate, endDate });
     const response = await axios.get(
       "https://localhost:7226/GetAllShiftsByRange",
       {
@@ -19,14 +19,14 @@ export const fetchShiftsByRange = async (
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      console.error("❌ Errore dettagliato:", {
+      console.error(" Errore dettagliato:", {
         message: error.message,
         response: error.response?.data,
         status: error.response?.status,
         config: error.config,
       });
     } else {
-      console.error("❌ Errore sconosciuto:", error);
+      console.error(" Errore sconosciuto:", error);
     }
     throw error;
   }
